e494487743e8659794e4b00df417259270c30fe3
Petko Petkov <ptpetkov666@gmail.com>
2018-04-18 17:38:24 +0300

    initial commit

---
+++ ExtensionMethod/ExtensionMethod/ExtensionMethodFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyExtension
+{
+    public static class ExtensionMethodFilter
+    {
+        public static IEnumerable<T> MyOwnFilter<T>
+            (this List<T> collection, Func<T, bool> input)
+        {
+            var output = new List<T>();
+            foreach (var item in collection)
+            {
+                if (input(item))
+                {
+                    output.Add(item);
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ ExtensionMethod/ExtensionMethod/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using MyExtension;
+namespace Seven
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var filteredList = list.MyOwnFilter(n => n % 2 == 0);
+            foreach (var item in filteredList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
