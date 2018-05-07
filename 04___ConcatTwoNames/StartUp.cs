21329a832615ea53358cd79fb52c8eb8a18dcb18
Petko Petkov <ptpetkov666@gmail.com>
2018-05-07 21:26:42 +0300

    concatAndValidateNames

---
+++ 04___ConcatTwoNames/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace ConcatTwoNames
+{
+    using System;
+    public class StartUp
+    {
+        static void Main()
+        {
+            Test name = new Test("Petko", "Petkov");
+            if (name.IsValid(name))
+            {
+                name.ConcatenameTwoNames();
+            }
+        }
+    }
+}
---
+++ 04___ConcatTwoNames/Test.cs
@@ -0,0 +1,40 @@
+﻿namespace ConcatTwoNames
+{
+    using System;
+    public class Test
+    {
+        private string firstName;
+        private string lastName;
+        public Test(string firsName, string lastName)
+        {
+            this.FirstName = firsName;
+            this.LastName = lastName;
+        }
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public bool IsValid(Test testNames)
+        {
+            if (char.IsUpper(testNames.firstName[0]) && char.IsUpper(testNames.lastName[0]))
+            {
+                return true;
+            }
+            return false;
+        }
+        public void ConcatenameTwoNames()
+        {
+            Console.WriteLine($"I am {this.FirstName} {this.LastName}");
+        }
+    }
+}
