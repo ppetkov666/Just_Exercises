f2c5778c80abf152664ae4c51dd8acc66f4b5924
Petko Petkov <ptpetkov666@gmail.com>
2018-05-09 12:36:38 +0300

    just a simple taks with 2 methods

---
+++ 06___IncreaseAgeOfAPerson/Person.cs
@@ -0,0 +1,30 @@
+﻿namespace IncreaseAgeOfaPerson
+{
+    class Person
+    {
+        private int age;
+        public Person(int age)
+        {
+            this.Age = age;
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public Person IncreaseAgeByOneYear()
+        {
+            age++;
+            return this;
+        }
+        public string TimeToRetire()
+        {
+            return "It is time for pension";
+        }
+        public void Print()
+        {
+            System.Console.WriteLine($"Person Age is: {age} !");
+        }
+    }
+}
---
+++ 06___IncreaseAgeOfAPerson/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace IncreaseAgeOfaPerson
+{
+    using System;
+    class StartUp
+    {
+        const int AgeForRetire = 65;
+        static void Main()
+        {
+            var person = new Person(66);
+            if (person.Age > AgeForRetire)
+            {
+                Console.WriteLine(person.TimeToRetire());
+                return;
+            }
+            person.IncreaseAgeByOneYear().Print();
+        }
+    }
+}
