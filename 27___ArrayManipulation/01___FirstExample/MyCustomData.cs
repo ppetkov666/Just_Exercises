e480cfdd28958440c61974bd51f3d3bde650d2ee
Petko Petkov <ptpetkov666@gmail.com>
2018-07-29 12:11:22 +0300

    array manipulation task

---
+++ 27___ArrayManipulation/01___FirstExample/MyCustomData.cs
@@ -0,0 +1,41 @@
+﻿namespace _01___FirstExample
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Text;
+    class MyCustomArray<T> : IEnumerable<T>
+    {
+        private T[] data;
+        public MyCustomArray(int size)
+        {
+            this.data = new T[size];
+        }
+        public int Length => this.data.Length;
+        public void Add(int position, T item)
+        {
+            data[position] = item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)data).GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.data.GetEnumerator();
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return this.data[index];
+            }
+            set
+            {
+                this.data[index] = value;
+            }
+        }
+    }
+}
---
+++ 27___ArrayManipulation/01___FirstExample/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _01___FirstExample
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //var customArray = new MyCustomArray<int>(20);
+            //customArray[0] = 10;
+            //customArray[1] = 20;
+            //customArray[2] = 30;
+            //customArray[3] = 40;
+            //customArray.Add(4, 400);
+            //for (int i = 0; i < customArray.Length; i++)
+            //{
+            //    if (customArray[i] > 0)
+            //    {
+            //        Console.WriteLine(customArray[i]);
+            //    }
+            //}
+            var numbers = new int[10];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i;
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + ", ");
+            }
+            var newArray = PetkoMethodToRemoveAt(numbers, 5);
+            Console.WriteLine();
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                Console.WriteLine(newArray[i]);
+            }
+        }
+        private static int[] PetkoMethodToRemoveAt(int[] myArray, int RemoveAt)
+        {
+            int[] newArray = new int[myArray.Length - 1];
+
+            int i = 0;
+            int j = 0;
+            while (i < myArray.Length)
+            {
+                if (i != RemoveAt)
+                {
+                    newArray[j] = myArray[i];
+                    j++;
+                }
+
+                i++;
+            }
+
+            return newArray;
+        }
+    }
+}
