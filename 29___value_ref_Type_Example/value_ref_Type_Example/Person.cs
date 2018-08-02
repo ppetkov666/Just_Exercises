54f35e82c0de639692fba240d4a7f8dca827fbb2
Petko Petkov <ptpetkov666@gmail.com>
2018-08-02 16:05:53 +0300

    another task exercise

---
+++ 29___value_ref_Type_Example/value_ref_Type_Example/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace example
+{
+    class Person
+    {
+        public Person spouse;
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+            // this is a great example for stackOverflow Exception
+            //this.spouse = new Person(string.Empty,0);
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
---
+++ 29___value_ref_Type_Example/value_ref_Type_Example/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace example
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("petko", 33);
+            // p i refference type and we just make r = p
+            Person r = p;
+            // when we print p.Age and Name , nothing unusual 
+            Console.WriteLine(p.Age);
+            Console.WriteLine(p.Name);
+            // because r = p and point the same address as p 
+            // once we change the name it will change it on both places
+            r.Name = "ivan";
+            Console.WriteLine(p.Age);
+            Console.WriteLine(p.Name);
+            Console.WriteLine(r.Age);
+            Console.WriteLine(r.Name);
+            var age = 55;
+            // when we assign the value of age to p.Age 
+            // we just copy the age because age in value type 
+            p.Age = age;
+            // and here when we change the value of age = 77 
+            // p.Age will remain the same !!!
+            age = 77;
+            Console.WriteLine(p.Age);
+
+            var personA = new Person("John", 44);
+            var personB = new Person("Edi", 55);
+            personA.spouse = personB;
+            Console.WriteLine(personA.spouse.Name);
+            Console.WriteLine(personA.spouse.Age);
+
+        }
+
+
+    }
+
+}
