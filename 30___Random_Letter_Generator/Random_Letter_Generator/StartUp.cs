533cc132ed4b2677266a9d9d808bfbda24bdc05b
Petko Petkov <ptpetkov666@gmail.com>
2018-08-03 14:53:30 +0300

    task - simple random letter generator

---
+++ 30___Random_Letter_Generator/Random_Letter_Generator/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace Random_Letter_Generator
+{
+    using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var randomClassExample = new Random();
+            var counter = 0;
+            for (char i = 'A'; i <= 'Z'; i++)
+            {
+
+                var number = randomClassExample.Next(1, 20);
+                if (number == 1)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+                else if (i == 'Z')
+                {
+                    var newnumber = randomClassExample.Next(1, 26);
+                    for (char a = 'A'; a <= 'Z'; a++)
+                    {
+                        counter++;
+                        if (counter == newnumber)
+                        {
+                            Console.WriteLine(a);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
