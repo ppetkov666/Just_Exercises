c35c9b0a645f567b25760f4a2744f12f0e229be4
Petko Petkov <ptpetkov666@gmail.com>
2018-08-10 17:14:48 +0300

    simple hack taks

---
+++ 31___SimpleHackTask/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleHackTask
+{
+    using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            // this task is how to switch between two states of bool variable without if statement 
+            // the comment ternary operator is if statement actually but it is just a another example how it can be done 
+            // the actual solution is  xor-equals true !!!
+            bool value = bool.Parse(Console.ReadLine());
+            value ^= true;
+            //value = (value) ? false : true;
+            Console.WriteLine(value);
+
+        }
+    }
+}
