eadd5a03b30c8d59a6597f09211c54b0cf3975ba
Petko Petkov <ptpetkov666@gmail.com>
2018-08-13 09:03:48 +0300

    restart PC app

---
+++ 32___ConsoleAppRestart/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleAppRestart
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo process = new ProcessStartInfo();
+            process.WindowStyle = ProcessWindowStyle.Hidden;
+            process.FileName = "cmd";
+            process.Arguments = "/C shutdown -f -r -t 5";
+            Process.Start(process);
+
+        }
+    }
+}
