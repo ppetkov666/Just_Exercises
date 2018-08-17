f6ea7237b48a63c24795fd05b6687abdf456887c
Petko Petkov <ptpetkov666@gmail.com>
2018-08-17 14:49:24 +0300

    Two Simple Tasks

---
+++ 36___TwoSimpleTasks/02___SecondTask/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace _02___SecondTask
+{
+    using System;
+
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            int maxnumber = int.MinValue;
+            int minnumber = int.MaxValue;
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                int parsedInput = int.Parse(input);
+                if (parsedInput > maxnumber)
+                {
+                    maxnumber = parsedInput;
+                }
+                if (parsedInput < minnumber)
+                {
+                    minnumber = parsedInput;
+                }
+            }
+            Console.WriteLine($"Max number: {maxnumber}");
+            Console.WriteLine($"Min number: {minnumber}");
+        }
+    }
+}
---
+++ 36___TwoSimpleTasks/TwoSimpleTasks/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace _01___FirstTask
+{
+    using System;
+
+    public class StartUp
+    {
+        public const string NAMEPRIVATE = "Petko";
+         
+        static void Main(string[] args)
+        {
+            var stringMessage = "hello {0},how are you ?";
+            PrintMessage(stringMessage, NAMEPRIVATE);
+
+        }
+        public static void PrintMessage(string first, string second)
+        {
+            Console.WriteLine(first, second);
+        }
+
+    }
+    
+}
