215a61f46f8fa15fb307f7aa46c82479c6115fa1
Petko Petkov <ptpetkov666@gmail.com>
2018-08-18 18:07:22 +0300

    two simple tasks

---
+++ 37___TwoSimpleTasks/01___PasswordGenerator/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _01___PasswordGenerator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var inputA = int.Parse(Console.ReadLine());
+            var inputB = char.Parse(Console.ReadLine().ToUpper());
+            var inputC = char.Parse(Console.ReadLine().ToLower());
+            var inputD = int.Parse(Console.ReadLine());
+            var inputE = int.Parse(Console.ReadLine());
+            var inputF = int.Parse(Console.ReadLine());
+            var n = long.Parse(Console.ReadLine());
+            long counter = 0;
+            bool istrue = true;
+            for (int a = 1; a <= inputA; a++)
+            {
+                for (char b = 'A'; b <= inputB; b++)
+                {
+                    for (char c = 'a'; c <= inputC; c++)
+                    {
+                        for (int d = 1; d <= inputD; d++)
+                        {
+                            for (int e = 1; e <= inputE; e++)
+                            {
+                                for (int f = 1; f <= inputF; f++)
+                                {
+                                    counter++;
+                                    if (counter == n)
+                                    {
+                                        istrue = false;
+                                        Console.WriteLine($"{a}{b}{c}{d}{e}{f}");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (istrue)
+            {
+                Console.WriteLine("No password on this position");
+            }
+        }
+    }
+}
---
+++ 37___TwoSimpleTasks/02___Char'sTransformation/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _02___Char_sTransformation
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int maxCodes = int.Parse(Console.ReadLine());
+            int count = 0;
+            var beginingM = 32;
+            var beginingN = 57;
+            for (int i = 1; i <= a; i++)
+            {
+                for (int y = 1; y <= b; y++)
+                {
+                    beginingM++;
+                    beginingN++;
+
+                    if (beginingM > 47)
+                    {
+                        beginingM = 33;
+                    }
+                    if (beginingN > 64)
+                    {
+                        beginingN = 58;
+                    }
+                    count++;
+                    Console.Write($"" +
+                        $"{(char)beginingM}" +
+                        $"{(char)beginingN}" +
+                        $"{i}" +
+                        $"{y}" +
+                        $"{(char)beginingN}" +
+                        $"{(char)beginingM}|");
+                    if (count >= maxCodes)
+                    {
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
