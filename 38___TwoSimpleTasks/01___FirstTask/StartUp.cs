ac46d43b4352ad8a3b42e7677b8d02273052af7c
Petko Petkov <ptpetkov666@gmail.com>
2018-08-20 17:06:08 +0300

    two simple tasks

---
+++ 38___TwoSimpleTasks/01___FirstTask/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01___FirstTask
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            // this task include optional parameter and 2 examples for 
+            // static and non static methods 
+            int first = 5;
+            int second = 7;
+            int third = 100;
+
+            StartUp petko = new StartUp();
+            int sumTwoParameters = petko.SumTwoNumbers(first, second);
+            Console.WriteLine(sumTwoParameters);
+
+            int sumTreeParameters = SumTwoNumbersV2(first, second, third);
+            Console.WriteLine(sumTreeParameters);
+        }
+        public int SumTwoNumbers(int first, int second, int third = 20)
+        {
+            var sum = first + second + third;
+            return sum;
+        }
+        public static int SumTwoNumbersV2(int first, int second, int third = 20)
+        {
+            var sum = first + second + third;
+            return sum;
+        }
+    }
+}
---
+++ 38___TwoSimpleTasks/02___SecondTask/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _02___SecondTask
+{
+    class StartUp
+    {
+        public const double GoodGrade = 4.0;
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                double sum = 0;
+                Boolean failed = false;
+                for (int i = 1; i <= 12; i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+
+                    if (grade < GoodGrade)
+                    {
+                        if (failed)
+                        {
+                            Console.WriteLine
+                            ($"{input} has been excluded at {i} grade");
+                            break;
+                        }
+                        failed = true;
+                        i--;
+                    }
+                    else
+                    {
+                        failed = false;
+                        sum += grade;
+                    }
+                }
+                if (failed)
+                {
+                    input = Console.ReadLine();
+                }
+                else
+                {
+                    double total = sum / 12;
+                    Console.WriteLine($"{input} graduated. Average grade: {total:f2}");
+                    input = Console.ReadLine();
+                }
+
+            }
+        }
+    }
+}
