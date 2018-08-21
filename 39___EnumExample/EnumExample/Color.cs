3403555be574bd414cd8f4662e912d63444f5848
Petko Petkov <ptpetkov666@gmail.com>
2018-08-21 14:51:29 +0300

    Enum example

---
+++ 39___EnumExample/EnumExample/Color.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NextExample
+{
+    
+    public enum Color
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ 39___EnumExample/EnumExample/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace MyNamespace
+{
+    using System;
+    using System.Data;
+    using NextExample;
+    
+    
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var colors = Enum.GetValues(typeof(Color));
+            var days = colors.GetValue(1);
+            Console.WriteLine(days + "---");
+            foreach (var item in colors)
+            {
+                Console.WriteLine(item);
+            }
+
+            var day = Color.Monday | Color.Tuesday;
+            if (day.HasFlag(Color.Wednesday))
+            {
+                Console.WriteLine("yes");
+            }
+            
+        }
+    }
+}
