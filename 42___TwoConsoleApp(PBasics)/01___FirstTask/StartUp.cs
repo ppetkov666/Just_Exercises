403d2d0019444b981c7aee2e12a615a504b28cdd
Petko Petkov <ptpetkov666@gmail.com>
2018-08-24 14:25:32 +0300

    two pBasics tasks

---
+++ 42___TwoConsoleApp(PBasics)/01___FirstTask/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _11.programing_Basics_Task
+{
+    class StartUp
+    {
+        public const int StepsGoal = 10000;
+        static void Main(string[] args)
+        {
+
+            string steps = Console.ReadLine();
+            int counter = 0;
+            int stepsAsInt;
+            while (steps != "Going home")
+            {
+                
+                bool isfalse = int.TryParse(steps, out stepsAsInt);
+                counter += stepsAsInt;
+                if (counter >= StepsGoal)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                    return;
+                }
+                steps = Console.ReadLine();
+            }
+            int lastSteps = int.Parse(Console.ReadLine());
+            counter += lastSteps;
+            if (counter >= StepsGoal)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                return;
+            }
+            else
+            {
+                int neededSteps = StepsGoal - counter;
+                Console.WriteLine($"{neededSteps} more steps to reach goal.");
+            }
+        }
+    }
+}
---
+++ 42___TwoConsoleApp(PBasics)/02___SecondTask/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+namespace _12___ProgramingBasicTask
+{
+    class StartUp
+    {
+        public const int DateCount = 2;
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int[] daysStorage = new int[DateCount];
+
+            for (int i = 0; i < DateCount; i++)
+            {
+                string input = Console.ReadLine();
+                string outputFormat = "dd-MM-yyyy";
+                DateTime parsedInput = DateTime.ParseExact(input, outputFormat, CultureInfo.InvariantCulture);
+                DateTime days = new DateTime();
+                int counter = 0;
+                if (i == 0)
+                {
+                    for (int a = 1; a <= number; a++)
+                    {
+                        if (a % 2 == 0)
+                        {
+                            counter++;
+                        }
+
+                    }
+                    days = parsedInput.AddDays(counter);
+                }
+                else
+                {
+                    for (int a = 1; a <= number; a++)
+                    {
+                        if (a % 2 != 0)
+                        {
+                            counter++;
+                        }
+
+                    }
+                    days = parsedInput.AddDays(counter);
+                }
+                daysStorage[i] = days.Day;
+            }
+            int sum = 0;
+            for (int i = 0; i < daysStorage.Length; i++)
+            {
+                sum += daysStorage[i];
+            }
+            Console.WriteLine(sum);
+           
+        }
+    }
+}
