c9c81735edb22b83d4be6fc072f62f5cd0e6ea16
Petko Petkov <ptpetkov666@gmail.com>
2018-08-25 21:03:30 +0300

    two tasks - delegates and events

---
+++ 43___TwoTasks_DelegatesAndEvents/01___DelegatesExample/StartUp.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace _02_DelegatesAndEvents
+{
+    using System;
+    public delegate void MyClass_Delegate();
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MyFirstTestClass firstClass = new MyFirstTestClass();
+            firstClass.Click = Hello;
+            firstClass.Click += HowAreYou;
+            firstClass.TestMethod();
+
+            MySecondClass.SecondStaticTestMethod(NiceToMeetYou);
+            
+        }
+        // these are three static methods just for the example purposes
+        private static void Hello() { Console.WriteLine("Hello"); }
+        private static void HowAreYou() { Console.WriteLine("How Are You"); }
+        private static void NiceToMeetYou() { Console.WriteLine("Nice To Meet You"); }
+    }
+    public class MyFirstTestClass
+    {
+        public MyClass_Delegate Click {get;set;} // member variable or field or instance variable from type Delegate
+
+        // this method has member variable from type delegate inside 
+        public void TestMethod()
+        {
+            Click();
+        }
+    }
+    public class MySecondClass
+    {
+        
+        // this method takes delegate as parameter
+        public static void SecondStaticTestMethod(MyClass_Delegate click)
+        {
+            click();
+        }
+    }
+}
---
+++ 43___TwoTasks_DelegatesAndEvents/02___EventsExample/StartUp.cs
@@ -0,0 +1,53 @@
+﻿
+namespace _03___Events
+{
+    using System;
+    public delegate void MyDelegate(string name);
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            MyFirstClass firstClassExample = new MyFirstClass();
+            firstClassExample.myEvent += Hello;
+            firstClassExample.myEvent += HowAreYou;
+            firstClassExample.TestMethod();
+
+            MySecondClass secondClassExample = new MySecondClass();
+            secondClassExample.myevent += SecondClassExample_myevent;
+            secondClassExample.TestMethod();
+
+        }
+
+        private static void SecondClassExample_myevent(object sender, EventArgs e)
+        {
+            NiceToMeetYou();
+        }
+
+        private static void Hello(string name) { Console.WriteLine($"Hello {name}"); }
+        private static void HowAreYou(string name) { Console.WriteLine($"How Are You {name}"); }
+        private static void NiceToMeetYou() { Console.WriteLine("Nice To Meet You"); }
+    }
+    public class MyFirstClass
+    {
+        public event MyDelegate myEvent;
+        public void TestMethod()
+        {
+            if (myEvent != null)
+            {
+                myEvent("petko");
+            }
+        }
+    }
+    public class MySecondClass
+    {
+        public event EventHandler<EventArgs> myevent;
+        public void TestMethod()
+        {
+            if (myevent != null)
+            {
+                myevent(this, new EventArgs());
+            }
+        }
+    }
+}
