039ee641b4699d9de098364f28943ec1da79d73d
Petko Petkov <ptpetkov666@gmail.com>
2018-08-30 16:50:49 +0300

    class library example

---
+++ 45___CustomClassLibraryExample/01___LibraryExample/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace _01___LibraryExample
+{
+    using System;
+    using MathOperations;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int firstnumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            MathOperations results = new MathOperations(firstnumber,secondNumber);
+            results.PrintTheSum();
+            results.PrintTheAbsDifference();
+            results.PrintMultiplyResult();
+            results.PrintDivisionResult();
+        }
+    }
+}
---
+++ 45___CustomClassLibraryExample/MathOperations/MathOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MathOperations
+{
+    public class MathOperations
+    {
+        public double FirstNumber { get; set; }
+        public double SecondNumber { get; set; }
+
+        public MathOperations(int firstNumber, int secondNumber)
+        {
+            FirstNumber = firstNumber;
+            SecondNumber = secondNumber;
+        }
+        public void PrintOnTheConsole(double result)
+        {
+            Console.WriteLine($"The Result is: {result:f2}");
+        }
+        public void PrintTheSum()
+        {
+            double result = FirstNumber + SecondNumber; 
+            PrintOnTheConsole(result);
+
+        }
+        public void PrintTheAbsDifference()
+        {
+            double result = Math.Abs(FirstNumber - SecondNumber);
+            PrintOnTheConsole(result);
+
+        }
+        public void PrintMultiplyResult()
+        {
+            double result = FirstNumber * SecondNumber;
+            PrintOnTheConsole(result);
+        }
+        public void PrintDivisionResult()
+        {
+            double result = (double)FirstNumber / SecondNumber;
+            PrintOnTheConsole(result);
+        }
+    }
+}
