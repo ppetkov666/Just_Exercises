27204782a0153d159784903abf59c70e41df3153
Petko Petkov <ptpetkov666@gmail.com>
2018-08-31 15:56:15 +0300

    same task as the day before but with library dll copied into the project

---
+++ 46___ReferenceASdll/ReferenceASdll/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace ReferenceASdll
+{
+    using System;
+    using MathOperations;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+            MathOperations petko = new MathOperations(5, 6);
+            petko.PrintTheSum();
+            petko.PrintTheAbsDifference();
+            petko.PrintMultiplyResult();
+            petko.PrintDivisionResult();
+        }
+    }
+}
