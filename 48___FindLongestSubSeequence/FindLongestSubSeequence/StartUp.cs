4a7284e6124b6a6cafe75462e963eec088937114
Petko Petkov <ptpetkov666@gmail.com>
2018-09-02 19:36:46 +0300

    task with custom Dll included

---
+++ 48___FindLongestSubSeequence/FindLongestSubSeequence/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace FindLongestSubSeequence
+{
+    using System;
+    using System.Linq;
+    using LongestIncreasingSubSequence;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var sequence = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine("{0}", string.Join(" ", FindLongestSubSequence.Get(sequence)));
+        }
+    }
+}
