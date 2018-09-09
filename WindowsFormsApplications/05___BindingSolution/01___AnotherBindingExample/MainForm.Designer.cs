a5df4a4a86453ca630805fb8db32155e192e64d6
Petko Petkov <ptpetkov666@gmail.com>
2018-09-09 22:54:33 +0300

    binding fields task

---
+++ WindowsFormsApplications/05___BindingSolution/01___AnotherBindingExample/MainForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace _03___AnotherBindingExample
+{
+    partial class CheckForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(436, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(129, 165);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(436, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // CheckForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 266);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "CheckForm";
+            this.Text = "CheckForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ WindowsFormsApplications/05___BindingSolution/01___AnotherBindingExample/MainForm.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace _03___AnotherBindingExample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+    public partial class CheckForm : Form
+    {
+
+        public CheckForm()
+        {
+            InitializeComponent();
+
+            //data source
+            Binding binding = new Binding("Text", textBox2, "Text", true, DataSourceUpdateMode.OnPropertyChanged);
+            // data target
+            // the goal is whatever is written in textBox1 to be updated on textBox2
+            textBox1.DataBindings.Add(binding);
+            // this could be written on One Row also 
+            //textBox1.DataBindings.Add("Text", textBox2, "text", true, DataSourceUpdateMode.OnPropertyChanged);
+
+            // the second interesting thing is :
+            this.DataBindings.Add("Size", textBox2, "Text", true, DataSourceUpdateMode.OnPropertyChanged);
+
+            //
+        }
+
+        private void CheckForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
