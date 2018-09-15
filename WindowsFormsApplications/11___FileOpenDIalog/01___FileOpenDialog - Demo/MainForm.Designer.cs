26a65d57005f1b852aa02aa11f53ec610bd9a3a2
Petko Petkov <ptpetkov666@gmail.com>
2018-09-15 14:40:05 +0300

    File open app

---
+++ WindowsFormsApplications/11___FileOpenDIalog/01___FileOpenDialog - Demo/MainForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace _10___
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.PanelBottom = new System.Windows.Forms.Panel();
+            this.ButtonLoadFile = new System.Windows.Forms.Button();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.PanelBottom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TextBox
+            // 
+            this.TextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TextBox.Location = new System.Drawing.Point(0, 0);
+            this.TextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TextBox.Multiline = true;
+            this.TextBox.Name = "TextBox";
+            this.TextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBox.Size = new System.Drawing.Size(1026, 709);
+            this.TextBox.TabIndex = 0;
+            // 
+            // PanelBottom
+            // 
+            this.PanelBottom.Controls.Add(this.ButtonLoadFile);
+            this.PanelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.PanelBottom.Location = new System.Drawing.Point(0, 461);
+            this.PanelBottom.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PanelBottom.Name = "PanelBottom";
+            this.PanelBottom.Size = new System.Drawing.Size(1026, 248);
+            this.PanelBottom.TabIndex = 1;
+            // 
+            // ButtonLoadFile
+            // 
+            this.ButtonLoadFile.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonLoadFile.AutoSize = true;
+            this.ButtonLoadFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonLoadFile.Location = new System.Drawing.Point(338, 85);
+            this.ButtonLoadFile.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ButtonLoadFile.Name = "ButtonLoadFile";
+            this.ButtonLoadFile.Size = new System.Drawing.Size(375, 77);
+            this.ButtonLoadFile.TabIndex = 0;
+            this.ButtonLoadFile.Text = "Load File";
+            this.ButtonLoadFile.UseVisualStyleBackColor = true;
+            this.ButtonLoadFile.Click += new System.EventHandler(this.ButtonLoadFile_Click);
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileName = "OpenFileDialog";
+            this.OpenFileDialog.Filter = "Text files (*.txt)|*.txt|Log files (*.log)|*.log";
+            this.OpenFileDialog.Title = "Choose text file";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 709);
+            this.Controls.Add(this.PanelBottom);
+            this.Controls.Add(this.TextBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.Text = "FileOpenDialog - Demo";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.PanelBottom.ResumeLayout(false);
+            this.PanelBottom.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.Panel PanelBottom;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.Button ButtonLoadFile;
+    }
+}
+
---
+++ WindowsFormsApplications/11___FileOpenDIalog/01___FileOpenDialog - Demo/MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10___
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLoadFile_Click(object sender, EventArgs e)
+        {
+            if (OpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = OpenFileDialog.FileName;
+                using (StreamReader reader = File.OpenText(fileName))
+                {
+                    string fileContent = reader.ReadToEnd();
+                    TextBox.Text = fileContent;
+                }
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
