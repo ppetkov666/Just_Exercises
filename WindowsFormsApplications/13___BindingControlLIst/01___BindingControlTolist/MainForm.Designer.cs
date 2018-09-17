d59cef27851fa531efe93c40cd760354a03f6895
Petko Petkov <ptpetkov666@gmail.com>
2018-09-17 13:37:39 +0300

    binding control to list

---
+++ WindowsFormsApplications/13___BindingControlLIst/01___BindingControlTolist/MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace _14___BindingControlTolist
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbTextBoxTowns = new System.Windows.Forms.TextBox();
+            this.ButtonPrev = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbTextBoxTowns
+            // 
+            this.tbTextBoxTowns.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTextBoxTowns.Location = new System.Drawing.Point(109, 69);
+            this.tbTextBoxTowns.Name = "tbTextBoxTowns";
+            this.tbTextBoxTowns.Size = new System.Drawing.Size(456, 32);
+            this.tbTextBoxTowns.TabIndex = 0;
+            // 
+            // ButtonPrev
+            // 
+            this.ButtonPrev.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonPrev.Location = new System.Drawing.Point(109, 160);
+            this.ButtonPrev.Name = "ButtonPrev";
+            this.ButtonPrev.Size = new System.Drawing.Size(202, 42);
+            this.ButtonPrev.TabIndex = 1;
+            this.ButtonPrev.Text = "Previous";
+            this.ButtonPrev.UseVisualStyleBackColor = true;
+            this.ButtonPrev.Click += new System.EventHandler(this.ButtonPrev_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonNext.Location = new System.Drawing.Point(363, 160);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(202, 42);
+            this.buttonNext.TabIndex = 2;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 263);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.ButtonPrev);
+            this.Controls.Add(this.tbTextBoxTowns);
+            this.Name = "MainForm";
+            this.Text = "Binding Control to List";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbTextBoxTowns;
+        private System.Windows.Forms.Button ButtonPrev;
+        private System.Windows.Forms.Button buttonNext;
+    }
+}
+
---
+++ WindowsFormsApplications/13___BindingControlLIst/01___BindingControlTolist/MainForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _14___BindingControlTolist
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string[] towns = new string[]
+            {
+                "Sofia",
+                "Plovdiv",
+                "Varna",
+                "Bourgas",
+                "Ruse",
+                "Stara zagora"
+            };
+            tbTextBoxTowns.DataBindings.Add("text", towns, "");
+        }
+
+        private void ButtonPrev_Click(object sender, EventArgs e)
+        {
+            CurrencyManager cm = (CurrencyManager)tbTextBoxTowns.DataBindings["Text"].BindingManagerBase;
+            if (cm.Position > 0)
+            {
+                cm.Position--;
+            }
+            else
+            {
+                cm.Position = cm.Count - 1;
+            }
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            CurrencyManager cm = (CurrencyManager)tbTextBoxTowns.DataBindings["Text"].BindingManagerBase;
+            if (cm.Position < cm.Count - 1)
+            {
+                cm.Position++;
+            }
+            else
+            {
+                cm.Position = 0;
+            }
+        }
+    }
+}
