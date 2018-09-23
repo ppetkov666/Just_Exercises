f56aea734e8a48c6bab32e7010b2089a7a8c7925
Petko Petkov <ptpetkov666@gmail.com>
2018-09-23 23:26:25 +0300

    simple data grid view table example

---
+++ WindowsFormsApplications/19___SimpleDataGridTableExample/01___SimpleDataGridTableExample/MainForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace _16___DataGrid
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataGridTowns = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridTowns)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataGridTowns
+            // 
+            this.DataGridTowns.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DataGridTowns.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridTowns.Location = new System.Drawing.Point(52, 34);
+            this.DataGridTowns.Name = "DataGridTowns";
+            this.DataGridTowns.ReadOnly = true;
+            this.DataGridTowns.Size = new System.Drawing.Size(247, 154);
+            this.DataGridTowns.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(408, 250);
+            this.Controls.Add(this.DataGridTowns);
+            this.Name = "MainForm";
+            this.Text = "Data Grid Demo";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridTowns)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.DataGridView DataGridTowns;
+    }
+}
+
---
+++ WindowsFormsApplications/19___SimpleDataGridTableExample/01___SimpleDataGridTableExample/MainForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _16___DataGrid
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Create table "Towns"
+            DataTable towns = new DataTable("Towns");
+            towns.Columns.Add(new DataColumn("id", typeof(int)));
+            towns.Columns.Add(new DataColumn("name", typeof(string)));
+            // Add some rows in the table
+            DataRow row = towns.NewRow();
+            row["id"] = 1;
+            row["name"] = "София";
+            towns.Rows.Add(row);
+            row = towns.NewRow();
+            row["id"] = 2;
+            row["name"] = "Пловдив";
+            towns.Rows.Add(row);
+            row = towns.NewRow();
+            row["id"] = 3;
+            row["name"] = "Варна";
+            towns.Rows.Add(row);
+            row = towns.NewRow();
+            row["id"] = 4; 
+            row["name"] = "Русе";
+            towns.Rows.Add(row);
+            row = towns.NewRow();
+            row["id"] = 5;
+            row["name"] = "Stara Zagora";
+            towns.Rows.Add(row);
+            // Add table "Towns" to the DataSet
+            DataSet ds = new DataSet();
+            ds.Tables.Add(towns);
+            // Bind the DataGrid to the DataSet
+            DataGridTowns.DataSource = ds;
+            DataGridTowns.DataMember = "Towns";
+        }
+    }
+}
