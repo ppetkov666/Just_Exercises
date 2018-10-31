a39bc7c5788abf28f225bab2a59bf8fd3cbb6130
Petko Petkov <ptpetkov666@gmail.com>
2018-10-31 16:00:09 +0200

    dynamically created labels and text boxes

---
+++ WindowsFormsApplications/24___DynamicallyCreatedTexBoxes/DynamicallyCreatedTexBoxes/MainForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace DynamicallyCreatedTexBoxes
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stripMainMenu = new System.Windows.Forms.MenuStrip();
+            this.itemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.DisplayPnl = new System.Windows.Forms.Panel();
+            this.stripMainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // stripMainMenu
+            // 
+            this.stripMainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itemFile});
+            this.stripMainMenu.Location = new System.Drawing.Point(0, 0);
+            this.stripMainMenu.Name = "stripMainMenu";
+            this.stripMainMenu.Size = new System.Drawing.Size(481, 24);
+            this.stripMainMenu.TabIndex = 0;
+            this.stripMainMenu.Text = "stripMainMenu";
+            // 
+            // itemFile
+            // 
+            this.itemFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itemOpen,
+            this.itemSaveAs});
+            this.itemFile.Name = "itemFile";
+            this.itemFile.Size = new System.Drawing.Size(37, 20);
+            this.itemFile.Text = "File";
+            // 
+            // itemOpen
+            // 
+            this.itemOpen.Name = "itemOpen";
+            this.itemOpen.Size = new System.Drawing.Size(111, 22);
+            this.itemOpen.Text = "Open";
+            this.itemOpen.Click += new System.EventHandler(this.itemOpen_Click);
+            // 
+            // itemSaveAs
+            // 
+            this.itemSaveAs.Enabled = false;
+            this.itemSaveAs.Name = "itemSaveAs";
+            this.itemSaveAs.Size = new System.Drawing.Size(111, 22);
+            this.itemSaveAs.Text = "SaveAs";
+            this.itemSaveAs.Click += new System.EventHandler(this.itemSaveAs_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "|*.txt";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.FileName = "sample";
+            this.saveFileDialog.Filter = "|*.txt";
+            // 
+            // DisplayPnl
+            // 
+            this.DisplayPnl.Location = new System.Drawing.Point(0, 27);
+            this.DisplayPnl.Name = "DisplayPnl";
+            this.DisplayPnl.Size = new System.Drawing.Size(477, 425);
+            this.DisplayPnl.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 457);
+            this.Controls.Add(this.DisplayPnl);
+            this.Controls.Add(this.stripMainMenu);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.stripMainMenu.ResumeLayout(false);
+            this.stripMainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip stripMainMenu;
+        private System.Windows.Forms.ToolStripMenuItem itemFile;
+        private System.Windows.Forms.ToolStripMenuItem itemOpen;
+        private System.Windows.Forms.ToolStripMenuItem itemSaveAs;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Panel DisplayPnl;
+    }
+}
+
---
+++ WindowsFormsApplications/24___DynamicallyCreatedTexBoxes/DynamicallyCreatedTexBoxes/MainForm.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DynamicallyCreatedTexBoxes
+{
+    public partial class MainForm : Form
+    {
+        private static List<TextBox> textBoxList;
+        private static List<Label> labelList;
+        private static string textToBeChanged;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void itemOpen_Click(object sender, EventArgs e)
+        {
+            //First we create list of parameters where we will store the text
+            List<string> parameters = new List<string>();
+            // we will catch everything between this open and closed brackets
+            string begin = "[[";
+            string end = "]]";
+            // when we click on Open - OpenFileDialog will be called and we will choose the file
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result != DialogResult.OK) return;
+            // if everything is ok we clear all controls if any exists
+            DisplayPnl.Controls.Clear();
+            // then we get the directory
+            string dir = openFileDialog.FileName;
+            //read all lines of text on string array
+            string[] lines = File.ReadAllLines(dir);
+            // and here we get all the text who we will need at the end of the program when we have to replace it
+            textToBeChanged = File.ReadAllText(dir);
+            //declare string variable which we will use later
+            string param;
+            // inthe foreach loop we go through all the lines of text and if there any between our[[  and ]] brackets
+            // we get this  text as param and add it to our list of parameters
+            foreach (var line in lines)
+            {
+                if (line.Contains(begin) && line.Contains(end))
+                {  
+                    int startIndex = line.IndexOf(begin);
+                    int endIndex = line.LastIndexOf(end);
+                    int length = (endIndex - startIndex) - 2 ;
+                    param = line.Substring(startIndex + 2, length);
+                    parameters.Add(param);
+                }
+            }
+            // after we have all the parameters we create 2 lists where we will keep  labels and text boxes which will be created dynamically
+            // and then added to controls of our DisplatPnl
+            textBoxList = new List<TextBox>();
+            labelList = new List<Label>();
+
+            for (int i = 0; i < parameters.Count; i++)
+            {
+                labelList.Add(CreateLabel(parameters[i], i));
+                textBoxList.Add(CreateTextBox(i));
+            }
+
+            for (int i = 0; i < parameters.Count; i++)
+            {
+                this.DisplayPnl.Controls.Add(textBoxList[i]);
+                this.DisplayPnl.Controls.Add(labelList[i]);
+            }
+            itemSaveAs.Enabled = true;
+        }
+
+        private void itemSaveAs_Click(object sender, EventArgs e)
+        {
+            DialogResult result = saveFileDialog.ShowDialog();
+            if (result != DialogResult.OK) return;
+            string filePath = saveFileDialog.FileName;
+            File.WriteAllText(filePath, SaveText(textToBeChanged));
+            string fileName = Path.GetFileName(filePath);
+            string message = string.Format("File {0} saved", fileName);
+            MessageBox.Show(message);
+        }
+
+        //label
+        private Label CreateLabel(string parameter, int index)
+        {
+            Label dynamicLabel = new Label();
+            dynamicLabel.Text = parameter;
+            dynamicLabel.Location = new Point(0, 10 + (index * 30));
+            dynamicLabel.Size = new Size(160, 30);
+            return dynamicLabel;
+        }
+
+        //TextBox
+        private TextBox CreateTextBox(int index)
+        {
+            TextBox dynamicTextBox = new TextBox();
+            dynamicTextBox.Location = new Point(180, 10 + (index * 30));
+            dynamicTextBox.Size = new Size(270, 22);
+            return dynamicTextBox;
+        }
+
+        private string SaveText(string text)
+        {
+            for (int i = 0; i < labelList.Count; i++)
+            {
+                if (text.Contains(labelList[i].Text))
+                {
+                    text = text.Replace(labelList[i].Text, textBoxList[i].Text);
+                }
+            }
+            return text;
+        }    
+    }
+}
