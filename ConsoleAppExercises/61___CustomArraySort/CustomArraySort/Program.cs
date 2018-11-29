5283797a926f3f737f696f1c332764f1876e213a
Petko Petkov <ptpetkov666@gmail.com>
2018-11-29 17:31:25 +0200

    custom array sort

---
+++ ConsoleAppExercises/61___CustomArraySort/CustomArraySort/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace CustomArraySort
+{
+    using System;
+    using System.Collections;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            var sortedLIst = CustomSort(input);
+            foreach (var item in sortedLIst)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+        public static IList CustomSort(IList arrayToSort)
+        {
+            int n = arrayToSort.Count - 1;
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = n; j > i; j--)
+                {
+                    if (((IComparable)arrayToSort[j - 1]).CompareTo(arrayToSort[j]) > 0)
+                    {
+                        object temp = arrayToSort[j - 1];
+                        arrayToSort[j - 1] = arrayToSort[j];
+                        arrayToSort[j] = temp;
+                    }
+                }
+            }
+            return arrayToSort;
+        }
+    }
+}
