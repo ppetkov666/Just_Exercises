94d433d7dcb83f09f56d77b90e57dcea9f7a2114
Petko Petkov <ptpetkov666@gmail.com>
2018-11-30 18:29:49 +0200

    bitwise operations - 2 simple tasks

---
+++ ConsoleAppExercises/62___BitwiseOperations_2 simple_tasks/02_Task/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02_Task
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int position = int.Parse(Console.ReadLine());
+            CustomBitFunk(number, position);
+        }
+        private static void CustomBitFunk(int number,int position)
+        {
+           
+
+            string firstP = Convert.ToString(number, 2).PadLeft(4, '0');
+            Console.WriteLine(firstP);
+            int mask = 1 << position;
+            string secondP = Convert.ToString(mask, 2).PadLeft(4, '0');
+            Console.WriteLine(secondP);
+            var result = number & mask;
+            string convertedResult = Convert.ToString(result, 2).PadLeft(4, '0');
+            Console.WriteLine(convertedResult);
+            result = result >> position;
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ ConsoleAppExercises/62___BitwiseOperations_2 simple_tasks/BitwiseOperations/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BitwiseOperations
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            CustomBitFunk(number);
+        }
+
+        private static void CustomBitFunk(int number)
+        {
+
+            string firstP = Convert.ToString(number, 2).PadLeft(4,'0');
+            Console.WriteLine(firstP);
+            int mask = 1 <<1;
+            string secondP = Convert.ToString(mask, 2).PadLeft(4, '0');
+            Console.WriteLine(secondP);
+            var result = number & mask;
+            string convertedResult = Convert.ToString(result, 2).PadLeft(4, '0');
+            result = result >> 1;
+            Console.WriteLine(result);
+
+        }
+    }
+}
