8c920ef6a9838a72ae0389d9476a55768130e382
Petko Petkov <ptpetkov666@gmail.com>
2018-12-02 22:13:02 +0200

    progr. fund exam task

---
+++ ConsoleAppExercises/63___Exam_taks_pr_f/Exam_taks/StartUp.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Exam_taks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoViews = new Dictionary<string, int>();
+            var videoLikes = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+            while (true)
+            {
+                if (input == "stats time")
+                {
+                    break;
+                }
+                if (input.Contains(":"))
+                {
+                    string[] inputArgs = input.Split(":").ToArray();
+                    string comment = inputArgs[0];
+                    string name = inputArgs[1];
+
+                    if (videoLikes.ContainsKey(name))
+                    {
+                        videoLikes[name] = comment == "like" ? videoLikes[name] += 1 : videoLikes[name] -= 1;
+
+                    }
+                    
+                }
+                if (input.Contains("-"))
+                {
+                    string[] inputArgs = input.Split("-").ToArray();
+                    string name = inputArgs[0];
+                    int views = int.Parse(inputArgs[1]);
+
+                    if (!videoViews.ContainsKey(name))
+                    {
+                        videoViews.Add(name, views);
+                        videoLikes.Add(name, 0);
+                    }
+                    else
+                    {
+                        videoViews[name] += views;
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            string orderCriteria = Console.ReadLine();
+            if (orderCriteria == "by likes")
+            {
+                videoViews = videoViews.OrderByDescending(x => videoLikes[x.Key])
+                                       .ToDictionary(x => x.Key, x => x.Value);
+                foreach (var videos in videoViews)
+                {
+                    Console.Write($"{videos.Key} - {videos.Value} views - ");
+
+                    var something = videoLikes.FirstOrDefault(x => x.Key == videos.Key).Value;
+                    Console.WriteLine($"{something} likes");
+                }
+            }
+            else
+            {
+                foreach (var video in videoViews.OrderByDescending(x => x.Value))
+                {
+                    Console.Write($"{video.Key} - {video.Value} views - ");
+
+                    var something = videoLikes.FirstOrDefault(x => x.Key == video.Key).Value;
+                    Console.WriteLine($"{something} likes");
+                }
+            }
+
+
+        }
+    }
+}
---
+++ ConsoleAppExercises/63___Exam_taks_pr_f/dictionary_simple_task/StartUp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace dictionary_simple_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, Dictionary<string, int>>();
+
+            while (true)
+            {
+                string data = Console.ReadLine();
+
+                if (data == "end")
+                {
+                    break;
+                }
+
+                string[] input = data.Split().ToArray();
+
+                string country = input[0];
+                string city = input[1];
+                int numberOfCitizens = int.Parse(input[2]);
+
+
+                if (dictionary.ContainsKey(country) == false)
+                {
+                    dictionary.Add(country, new Dictionary<string, int>());
+                }
+
+                if (dictionary[country].ContainsKey(city) == false)
+                {
+
+                    dictionary[country][city] = numberOfCitizens;
+                }
+
+            }
+
+
+
+
+            foreach (var state in dictionary )
+            {
+                Console.WriteLine($"{state.Key}");
+
+                foreach (var kvp in state.Value)
+                {
+                    string town = kvp.Key;
+                    int residents = kvp.Value;
+
+                    if (residents > 10000)
+                    {
+                        Console.WriteLine($"{town} - {residents}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{town}");
+                    }
+                }
+            }
+
+        }
+    }
+}
