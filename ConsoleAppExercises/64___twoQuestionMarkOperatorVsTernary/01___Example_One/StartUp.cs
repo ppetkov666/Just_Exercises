0dfb0c4865d96a0110fa4f9ec965bdf25f313ba4
Petko Petkov <ptpetkov666@gmail.com>
2018-12-07 18:06:32 +0200

    simple console app -ternary operator vs coalesce

---
+++ ConsoleAppExercises/64___twoQuestionMarkOperatorVsTernary/01___Example_One/StartUp.cs
@@ -0,0 +1,41 @@
+﻿
+namespace twoQuestionMarkOperatorVsTernary
+{
+    using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            // i will demonstrate 3 different cases just to show the output 
+            // of ternary operator and "??" operator comparing one another 
+            string ternaryResult = null;
+            string twoQuestionMarkResult = null;
+            string test = Console.ReadLine();
+
+            if (test == string.Empty)
+            {
+                ternaryResult = test != null ? "ternary operator: is it empthy string" : "ternary operator: null string";
+                twoQuestionMarkResult = test ?? "?? operator: null string";
+                PrintOutput(ternaryResult, twoQuestionMarkResult);
+            }
+            else if (test == "just regular string")
+            {
+                ternaryResult = test != null ? "ternary operator: just regular string" : "ternary operator: null string";
+                twoQuestionMarkResult = test ?? "?? operator: null string";
+                PrintOutput(ternaryResult, twoQuestionMarkResult);
+            }
+            else
+            {
+                test = null;
+                ternaryResult = test != null ? "ternary operator: not null string" : "ternary operator: null string";
+                twoQuestionMarkResult = test ?? "?? operator: null string";
+                PrintOutput(ternaryResult, twoQuestionMarkResult);
+            }
+        }
+        private static void PrintOutput(string ternaryResult, string twoQuestionMarkResult)
+        {
+            Console.WriteLine(ternaryResult);
+            Console.WriteLine(twoQuestionMarkResult);
+        }
+    }
+}
---
+++ ConsoleAppExercises/64___twoQuestionMarkOperatorVsTernary/02___Example_Two/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _02___Example_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Coaleascing Operator 
+            string first = null;
+            string second = null;
+            string third = null;
+            string fourth = "not empthy string";
+            string finalResult = first ?? second ?? third ?? fourth;
+            Console.WriteLine(finalResult);
+
+           // this code firther down just demonstrate how many if else statement we have type if we dont use 
+           // Coalescing Operator
+           //if (first != null)
+           //{
+           //    finalResult = first;
+           //}
+           //else
+           //{
+           //    if (second != null)
+           //    {
+           //        finalResult = second;
+           //    }
+           //    else
+           //    {
+           //        if (third != null)
+           //        {
+           //            finalResult = third;
+           //        }
+           //        else
+           //        {
+           //            if (fourth != null)
+           //            {
+           //                finalResult = fourth;
+           //            }
+           //        }
+           //    }
+           //}
+           //Console.WriteLine(finalResult);
+        }
+    }
+}
