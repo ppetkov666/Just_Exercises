4e5e27f8b907de24f83f0acc22707ae3e7b2fa42
Petko Petkov <ptpetkov666@gmail.com>
2018-12-15 16:06:50 +0200

    regex simple task about taking numeric value of chars

---
+++ ConsoleAppExercises/65___Regex_Task/Regex_Task/StartUp.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Regex_Task
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            string regex = @"<([a-z][0-9]*)([a-z][0-9]*)*>";
+
+            Regex washingTheDishesPattern = new Regex(regex);
+
+            int washingDishesTime = 0;
+
+            while (command != "wife is happy")
+            {
+
+                Match dishesMatch = washingTheDishesPattern.Match(command);
+
+                if (dishesMatch.Success)
+                {
+                    string array = dishesMatch.Value;
+                    char[] converted = array.ToCharArray();
+                    for (int i = 0; i < converted.Length; i++)
+                    {
+                        char letter = converted[i];
+                        if (char.IsDigit(letter))
+                        {
+                            int numericValue = (int)Char.GetNumericValue(letter);
+                            washingDishesTime += numericValue;
+                        }
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Doing the dishes {washingDishesTime}");
+        }
+    }
+}
