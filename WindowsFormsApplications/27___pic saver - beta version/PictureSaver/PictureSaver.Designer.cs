7719196901f557dac1fea9e3479a90fb50057b0f
Petko Petkov <ptpetkov666@gmail.com>
2019-01-13 21:29:15 +0200

    pic saver -beta version

---
+++ WindowsFormsApplications/27___pic saver - beta version/PictureSaver/PictureSaver.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace PictureSaver
+{
+    partial class PicSvrFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LnkTb = new System.Windows.Forms.TextBox();
+            this.SvDlBtn = new System.Windows.Forms.Button();
+            this.FldrDlg = new System.Windows.Forms.FolderBrowserDialog();
+            this.UrlLbl = new System.Windows.Forms.Label();
+            this.AlFlsPrgBr = new System.Windows.Forms.ProgressBar();
+            this.FilePrgsLbl = new System.Windows.Forms.Label();
+            this.OneFlPrgBr = new System.Windows.Forms.ProgressBar();
+            this.FileNmLbl = new System.Windows.Forms.Label();
+            this.PrgsLbl = new System.Windows.Forms.Label();
+            this.FileLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LnkTb
+            // 
+            this.LnkTb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LnkTb.Location = new System.Drawing.Point(12, 58);
+            this.LnkTb.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.LnkTb.Name = "LnkTb";
+            this.LnkTb.Size = new System.Drawing.Size(505, 22);
+            this.LnkTb.TabIndex = 0;
+            // 
+            // SvDlBtn
+            // 
+            this.SvDlBtn.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.SvDlBtn.Location = new System.Drawing.Point(157, 138);
+            this.SvDlBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.SvDlBtn.Name = "SvDlBtn";
+            this.SvDlBtn.Size = new System.Drawing.Size(215, 53);
+            this.SvDlBtn.TabIndex = 1;
+            this.SvDlBtn.Text = "Save/Download";
+            this.SvDlBtn.UseVisualStyleBackColor = true;
+            this.SvDlBtn.Click += new System.EventHandler(this.SvDlBtn_Click);
+            // 
+            // UrlLbl
+            // 
+            this.UrlLbl.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.UrlLbl.AutoSize = true;
+            this.UrlLbl.Location = new System.Drawing.Point(223, 25);
+            this.UrlLbl.Name = "UrlLbl";
+            this.UrlLbl.Size = new System.Drawing.Size(96, 17);
+            this.UrlLbl.TabIndex = 2;
+            this.UrlLbl.Text = "Input Url Here";
+            // 
+            // AlFlsPrgBr
+            // 
+            this.AlFlsPrgBr.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AlFlsPrgBr.Location = new System.Drawing.Point(12, 318);
+            this.AlFlsPrgBr.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.AlFlsPrgBr.Name = "AlFlsPrgBr";
+            this.AlFlsPrgBr.Size = new System.Drawing.Size(507, 23);
+            this.AlFlsPrgBr.Step = 1;
+            this.AlFlsPrgBr.TabIndex = 3;
+            this.AlFlsPrgBr.Click += new System.EventHandler(this.AlFlsPrgBr_Click);
+            // 
+            // FilePrgsLbl
+            // 
+            this.FilePrgsLbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FilePrgsLbl.AutoSize = true;
+            this.FilePrgsLbl.BackColor = System.Drawing.Color.Transparent;
+            this.FilePrgsLbl.Location = new System.Drawing.Point(155, 297);
+            this.FilePrgsLbl.Name = "FilePrgsLbl";
+            this.FilePrgsLbl.Size = new System.Drawing.Size(238, 17);
+            this.FilePrgsLbl.TabIndex = 4;
+            this.FilePrgsLbl.Text = "0% Completed 0/0 Files Downloaded";
+            // 
+            // OneFlPrgBr
+            // 
+            this.OneFlPrgBr.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OneFlPrgBr.Location = new System.Drawing.Point(12, 271);
+            this.OneFlPrgBr.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.OneFlPrgBr.Name = "OneFlPrgBr";
+            this.OneFlPrgBr.Size = new System.Drawing.Size(507, 23);
+            this.OneFlPrgBr.TabIndex = 5;
+            // 
+            // FileNmLbl
+            // 
+            this.FileNmLbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FileNmLbl.AutoSize = true;
+            this.FileNmLbl.BackColor = System.Drawing.Color.Transparent;
+            this.FileNmLbl.Location = new System.Drawing.Point(155, 251);
+            this.FileNmLbl.Name = "FileNmLbl";
+            this.FileNmLbl.Size = new System.Drawing.Size(138, 17);
+            this.FileNmLbl.TabIndex = 6;
+            this.FileNmLbl.Text = ".jpg/jpeg/gif/img/bmp";
+            // 
+            // PrgsLbl
+            // 
+            this.PrgsLbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PrgsLbl.AutoSize = true;
+            this.PrgsLbl.Location = new System.Drawing.Point(12, 297);
+            this.PrgsLbl.Name = "PrgsLbl";
+            this.PrgsLbl.Size = new System.Drawing.Size(65, 17);
+            this.PrgsLbl.TabIndex = 8;
+            this.PrgsLbl.Text = "Progress";
+            // 
+            // FileLbl
+            // 
+            this.FileLbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FileLbl.AutoSize = true;
+            this.FileLbl.Location = new System.Drawing.Point(12, 251);
+            this.FileLbl.Name = "FileLbl";
+            this.FileLbl.Size = new System.Drawing.Size(30, 17);
+            this.FileLbl.TabIndex = 9;
+            this.FileLbl.Text = "File";
+            // 
+            // PicSvrFrm
+            // 
+            this.AcceptButton = this.SvDlBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 359);
+            this.Controls.Add(this.FileLbl);
+            this.Controls.Add(this.PrgsLbl);
+            this.Controls.Add(this.FileNmLbl);
+            this.Controls.Add(this.OneFlPrgBr);
+            this.Controls.Add(this.FilePrgsLbl);
+            this.Controls.Add(this.AlFlsPrgBr);
+            this.Controls.Add(this.UrlLbl);
+            this.Controls.Add(this.SvDlBtn);
+            this.Controls.Add(this.LnkTb);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "PicSvrFrm";
+            this.Text = "Picture Saver";
+            this.Load += new System.EventHandler(this.PicSvrFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LnkTb;
+        private System.Windows.Forms.Button SvDlBtn;
+        private System.Windows.Forms.FolderBrowserDialog FldrDlg;
+        private System.Windows.Forms.Label UrlLbl;
+        private System.Windows.Forms.ProgressBar AlFlsPrgBr;
+        private System.Windows.Forms.Label FilePrgsLbl;
+        private System.Windows.Forms.ProgressBar OneFlPrgBr;
+        private System.Windows.Forms.Label FileNmLbl;
+        private System.Windows.Forms.Label PrgsLbl;
+        private System.Windows.Forms.Label FileLbl;
+    }
+}
+
---
+++ WindowsFormsApplications/27___pic saver - beta version/PictureSaver/PictureSaver.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PictureSaver
+{
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Forms;
+    using System.IO;
+    using System.Threading;
+    using System.Net;
+    using System.Xml;
+
+    public partial class PicSvrFrm : Form
+    {
+        private WebClient client;
+        private Queue<string> queueFiles;
+        private string fileNameFinal;
+
+        public PicSvrFrm()
+        {
+            InitializeComponent();
+        }
+
+        private bool ValidateUrl(string url)
+        {
+            char[] unacceptionals = new char[3] { '\\', '/', ' ' };
+
+            if (url.StartsWith("\\") || url.StartsWith("/") || url.StartsWith(" "))
+            {
+                url = url.TrimStart(unacceptionals);
+            }
+            else if (url.EndsWith("\\") || url.EndsWith(" "))
+            {
+                url = url.TrimEnd(unacceptionals);
+            }
+
+            else if (!url.Contains('.'))
+            {
+                MessageBox.Show("Invalid Url!");
+                return false;
+            }
+            return true;
+        }
+
+        private List<string> ParseHtml(string url)
+        {
+            //https://www.kinoarena.com
+            int first;
+            int last;
+            string tag = string.Empty;
+            List<string> tags = new List<string>();
+            string htmlString = string.Empty;
+            try
+            {
+                htmlString = client.DownloadString(url);
+            }
+            catch (WebException)
+            {
+                MessageBox.Show("Address cannot be accessed or does not exist!");
+                return tags;
+            }
+            for (int i = 0; i < htmlString.Length; i++)
+            {
+                first = htmlString.IndexOf("<img", i);
+                if (first < 0)
+                    break;
+                last = htmlString.IndexOf('>', first);
+                tag = htmlString.Substring(first, last - first + 1);
+                if (tag.Contains("http"))
+                {
+                    tags.Add(tag);
+                }
+                i = last;
+            }
+            return tags;
+        }
+
+        private Queue<string> ParseImageTags(List<string> imagTags)
+        {
+            int first;
+            int last;
+            string imgUrl = string.Empty;
+            Queue<string> imageUrls = new Queue<string>();
+
+            //for (int i = 0; i < imagTags.Count - 1; i++)
+            //{
+            //    if (imagTags[i].Contains("http"))
+            //    {
+            //        first = imagTags[i].IndexOf("http");
+            //        if (imagTags[i][first - 1] == '\"')
+            //        {
+            //            last = imagTags[i].IndexOf("\"", first);
+            //        }
+            //        else if (imagTags[i][first - 1] == '\'')
+            //        {
+            //            last = imagTags[i].IndexOf("\'", first);
+            //        }
+            //        else
+            //        {
+            //            last = imagTags[i].LastIndexOf(".");
+            //            last = last + 4;
+            //        }
+            //        imgUrl = imagTags[i].Substring(first, last - first);
+            //    }
+            //    imageUrls.Enqueue(imgUrl);
+            //}
+
+            foreach (string imgTag in imagTags)
+            {
+                int startIndex = imgTag.IndexOf("src=");
+                //<img  class="lazyOverHome" data-original=
+                //"http://m3.netinfo.bg/media/images/26151/26151085/130-130-iaponka.jpg" src="" width="130" height="130" alt="РЇРїР...
+
+                if (imgTag.Contains("http") && /*DOBAVKA*/(imgTag.IndexOf("http") > startIndex)/*DOBAVKA*/)
+                {
+                    try
+                    {
+                        first = imgTag.IndexOf('\"', startIndex);
+                        imgUrl = imgTag.Remove(0, first + 1);
+                        last = imgUrl.IndexOf('\"');
+                        imgUrl = imgUrl.Remove(last, imgUrl.Length - last);
+                    }
+                    catch (Exception)
+                    {
+                        continue;
+                    }
+                    if (Path.HasExtension(imgUrl))//DOBAVKA
+                    {
+                        imageUrls.Enqueue(imgUrl);
+                    }
+                }
+                else
+                {
+                    continue;
+                }//DOBAVKA
+            }
+            return imageUrls;
+        }
+        private void SvDlBtn_Click(object sender, EventArgs e)
+        {
+            client = new WebClientWithTimeout();
+            string urlInTb = LnkTb.Text;
+            DialogResult res = FldrDlg.ShowDialog();
+            if (res != DialogResult.OK)
+            {
+                return;
+            }
+            //vuv func - bool validateUrl(string url)
+            if (!ValidateUrl(urlInTb))
+            {
+                return;
+            }
+
+            if (!urlInTb.StartsWith("http"))
+            {
+                try
+                {
+                    urlInTb = new UriBuilder(urlInTb).Uri.ToString();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Invalid Url!");
+                    return;
+                }
+            }
+
+            List<string> imagesTags = ParseHtml(urlInTb);
+            if (imagesTags.Count == 0)
+            {
+                MessageBox.Show("Page does not contain any Images");
+                return;
+            }
+            queueFiles = ParseImageTags(imagesTags);
+            AlFlsPrgBr.Maximum = queueFiles.Count;
+            AlFlsPrgBr.Value = 0;
+            StartDownloading();
+        }
+        private string GetFileName(string imageUrl)
+        {
+            int question = imageUrl.IndexOf('?');
+            if (imageUrl.Contains('?'))
+            {
+                imageUrl = imageUrl.Remove(question);
+            }
+            string fileName = FldrDlg.SelectedPath + "\\" + Path.GetFileName(imageUrl);
+            string ext = Path.GetExtension(fileName);
+            int incresement = 0;
+            while (File.Exists(fileName))
+            {
+                fileName = FldrDlg.SelectedPath + "\\" + Path.GetFileNameWithoutExtension(imageUrl) + "(" + incresement + ")" + ext;
+                incresement++;
+            }
+            fileNameFinal = fileName;
+            return fileName;
+        }
+
+        private void StartDownloading()
+        {
+            SvDlBtn.Enabled = false;
+            client = new WebClient();
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            NextFile();
+        }
+        private void NextFile()
+        {
+            if (queueFiles.Count == 0)
+            {
+                MessageBox.Show("Download Complete!");
+                SvDlBtn.Enabled = true;
+                return;
+            }
+            AlFlsPrgBr.Value = AlFlsPrgBr.Value + 1;
+            string imageUrl = queueFiles.Dequeue();
+            string fileName = GetFileName(imageUrl);
+            client.DownloadFileAsync(new Uri(imageUrl), fileName);
+        }
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            OneFlPrgBr.Value = e.ProgressPercentage;
+            FileNmLbl.Text = Path.GetFileName(fileNameFinal) + "   " + OneFlPrgBr.Value.ToString() + "% Completed";
+            FilePrgsLbl.Text = AlFlsPrgBr.Value.ToString() + " / " + AlFlsPrgBr.Maximum.ToString() + " Files Downloaded";
+        }
+
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            NextFile();
+        }
+
+        private void PicSvrFrm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AlFlsPrgBr_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class WebClientWithTimeout : WebClient
+    {
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest wr = base.GetWebRequest(address);
+            wr.Timeout = 5000;
+            return wr;
+        }
+    }
+}
