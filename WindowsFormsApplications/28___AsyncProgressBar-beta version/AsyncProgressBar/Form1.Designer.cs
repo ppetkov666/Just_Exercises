3dd28874f4c34bf0dd69ed9b8bd3a0b683988056
Petko Petkov <ptpetkov666@gmail.com>
2019-01-17 17:23:55 +0200

    async progress bar

---
+++ WindowsFormsApplications/28___AsyncProgressBar-beta version/AsyncProgressBar/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace AsyncProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(61, 45);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(97, 40);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 137);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(442, 40);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(236, 45);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(107, 40);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 217);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ WindowsFormsApplications/28___AsyncProgressBar-beta version/AsyncProgressBar/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncProgressBar
+{
+    public partial class Form1 : Form
+    {
+        WebClient test = new WebClient();
+        CancellationTokenSource myToken;
+        Progress<int> myprogress;
+        int Counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            Counter = 0;
+            myToken = new CancellationTokenSource();
+            await startProcessBar(myToken);
+        }
+        private void ReportProgress(int myInt)
+        {
+            label1.Text = "Download " + myInt.ToString();
+            progressBar1.Value = myInt;
+        }
+
+        private async Task startProcessBar(CancellationTokenSource myToken)
+        {
+            Action<int> progressTarget = new Action<int>(ReportProgress);
+            myprogress = new Progress<int>(progressTarget);
+            for (int i = 0; i < 10; i++)
+            {
+                if (myToken.IsCancellationRequested)
+                {
+                    return;
+                }
+                string myString = await RetriveString(myprogress);
+            }
+        }
+        private async Task<string> RetriveString(IProgress<int> myprogress)
+        {
+            string myString = "";
+            if (myprogress != null)
+            {
+                Counter = Counter + 10;
+                myprogress.Report(Counter);
+            }
+            test.DownloadFile("https://softuni.bg/", "D:\\target_file.txt");
+            myString = await test.DownloadStringTaskAsync("https://softuni.bg/");
+            return myString;
+        }
+
+        private void btnStop_Click_1(object sender, EventArgs e)
+        {
+            if (myToken != null)
+            {
+                myToken.Cancel();
+            }
+        }
+    }
+}
