24d954049cde50b2eeb657aab3f9b800c227cd24
Petko Petkov <ptpetkov666@gmail.com>
2019-01-23 18:48:51 +0200

    thread console task

---
+++ ConsoleAppExercises/70___Thread_and_Callback/Thread_and_Callback/Calculate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Thread_and_Callback
+{
+    public class Calculate
+    {
+        int _number;
+        public CalculateNumbers _calculate;
+        public Calculate(int number, CalculateNumbers calculate)
+        {
+            this._number = number;
+            this._calculate = calculate;
+        }
+
+        public void CalculateSum()
+        {
+            int sum = 0;
+            for (int i = 1; i <= _number; i++)
+            {
+                sum += i;
+            }
+            Thread.Sleep(3000);
+            _calculate(sum);
+        }
+    }
+}
---
+++ ConsoleAppExercises/70___Thread_and_Callback/Thread_and_Callback/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_and_Callback
+{
+    public delegate void CalculateNumbers(int numbers);
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int number = int.Parse(Console.ReadLine());
+            CalculateNumbers callback = new CalculateNumbers(CallbackCalculate);
+            Calculate calculateNum = new Calculate(number, callback);
+            Thread thread = new Thread(calculateNum.CalculateSum);
+            thread.Start();
+            string text = "Please wait while your request is being proceeded";
+            Console.WriteLine(text);
+        }
+        public static void CallbackCalculate(int num)
+        {
+            Console.WriteLine($"The sum of numbers is {num.ToString()}"); 
+            
+        }
+    }
+}
