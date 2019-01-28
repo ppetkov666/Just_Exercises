88c59b481814a3de72810348d623c96186795bbf
Petko Petkov <ptpetkov666@gmail.com>
2019-01-28 18:05:59 +0200

    async await app loading data update

---
+++ WindowsFormsApplications/33___Async_Await_App_loading_data/Async_Await_App_loading_data/Form1.cs
@@ -0,0 +1,132 @@
+﻿namespace Async_Await_App_loading_data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Threading;
+    using System.Windows.Forms;
+    public partial class Form1 : Form
+    {
+        CancellationTokenSource cts = new CancellationTokenSource();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_execute_sync_Click(object sender, EventArgs e)
+        {
+            ListBoxClear(); 
+            Stopwatch watch = Stopwatch.StartNew();
+            // these are two different options
+            // the first one is with Parallel ForEach
+            //List<WebsiteDataModel> result = Methods.RunDownloadParallelSync();
+            // the second option is with normal foreach 
+            List<WebsiteDataModel> result = Methods.RunDownloadSync(prb_my_custom_loading);
+            PrintResults(result);
+
+            watch.Stop();
+            Int64 elapsedTimeInMiliSec = watch.ElapsedMilliseconds;
+            listBox1.Items.Add($"Total execution time: {elapsedTimeInMiliSec}");   
+        }
+
+        private async void Btn_async_execute_Click(object sender, EventArgs e)
+        {
+            ListBoxClear();
+            string text = btn_async_execute.Text;
+            ButtonLock(btn_async_execute);
+            cts.Dispose();
+            cts = new CancellationTokenSource();
+
+            Stopwatch watch = Stopwatch.StartNew();
+            // because of the purpose of this task i practice to handle the progress  in the background with default
+            // BackgroundWorker and with another Progress class. If you want to see the behavior of the other Progress you just 
+            // have to uncomment the code related with the instance of this class
+            try
+            {
+                List<WebsiteDataModel> result = await Methods.RunDownloadAsync(/*progress,*/prb_my_custom_loading, backgroundWorker1, cts.Token);
+                PrintResults(result);
+            }
+            catch (OperationCanceledException)
+            {                
+                listBox1.Items.Add($"The operation was cancelled!");
+            }
+
+            watch.Stop();
+            Int64 elapsedTimeInMiliSec = watch.ElapsedMilliseconds;
+            listBox1.Items.Add($"Total execution time: {elapsedTimeInMiliSec}");
+            
+            ButtonLock(btn_async_execute, text);
+        }
+
+        private async void Btn_parallel_async_Click(object sender, EventArgs e)
+        {
+            ListBoxClear();
+            string text = btn_parallel_async.Text;
+            ButtonLock(btn_parallel_async);
+            prb_my_custom_loading.Maximum = 100;
+            prb_my_custom_loading.Value = 0;
+
+            Stopwatch watch = Stopwatch.StartNew();
+
+            // here i also implemented 2 diff options: parallell foreach 
+            List<WebsiteDataModel> result = await Methods.RunDownloadParallelASyncV2(prb_my_custom_loading,backgroundWorker1);
+            // and adding all async tasks into a list of tasks. and executed them
+            //List<WebsiteDataModel> result = await Methods.RunDownloadParallelAsync();
+            PrintResults(result);
+
+            watch.Stop();
+            long elapsedTimeInMiliSec = watch.ElapsedMilliseconds;
+            listBox1.Items.Add($"Total execution time: {elapsedTimeInMiliSec}");
+            ButtonLock(btn_parallel_async, text);
+        }
+
+        private void Btn_cancel_Click(object sender, EventArgs e)
+        {
+            cts.Cancel();
+        }
+
+
+        private void PrintResults(List<WebsiteDataModel> results)
+        {
+            listBox1.Items.Clear();
+            foreach (var result in results)
+            {
+                listBox1.Items.Add($"{ result.WebsiteUrl } downloaded: { result.WedSiteData.Length } characters long.");
+            }
+        }
+
+        private void BackgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            prb_my_custom_loading.Value = e.ProgressPercentage;
+            ProgressReportModel prm = (ProgressReportModel)e.UserState;
+            PrintResults(prm.WebPagesDownloaded);
+
+        }
+
+        private void Progress_ProgressChanged(object sender, ProgressReportModel e)
+        {
+            PrintResults(e.WebPagesDownloaded);
+        }
+
+        private void ButtonLock(Button button, string text ="")
+        {
+            if (button.Enabled == true)
+            {
+                button.Enabled = false;
+                button.Text = "LOADING...Please Wait...";
+            }
+            else
+            {
+                button.Enabled = true;
+                button.Text = text;
+            }
+        }
+
+        private void ListBoxClear()
+        {
+            listBox1.Items.Clear();
+            listBox1.Refresh();
+        }
+    }
+}
---
+++ WindowsFormsApplications/33___Async_Await_App_loading_data/Async_Await_App_loading_data/Methods.cs
@@ -0,0 +1,165 @@
+﻿
+
+namespace Async_Await_App_loading_data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Drawing;
+    using System.Net;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    public class Methods
+    {
+        public static List<WebsiteDataModel> RunDownloadSync(ColorProgressBar.ColorProgressBar prb_loading)
+        {
+            List<string> webPages = Preparedata();
+
+            prb_loading.Visible = true;
+            prb_loading.BarColor = Color.FromArgb(255, 128, 255);
+            prb_loading.Maximum = webPages.Count;
+            prb_loading.Value = 0;
+            prb_loading.FillStyle = ColorProgressBar.ColorProgressBar.FillStyles.Dashed;
+
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+
+            foreach (var webpage in webPages)
+            {
+                prb_loading.Invoke((MethodInvoker)delegate
+                {
+                    prb_loading.Value += 1;
+                    prb_loading.Refresh();
+                });
+
+                WebsiteDataModel result = DownloadWebContent(webpage);
+                output.Add(result);
+            }
+            return output;
+        }
+
+        public static List<WebsiteDataModel> RunDownloadParallelSync()
+        {
+            List<string> webPages = Preparedata();
+
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+            // it works Syncroniously JUST executes the foreach in as many thread as there are processors available
+            Parallel.ForEach<string>(webPages, (site) =>
+             {
+                 WebsiteDataModel result = DownloadWebContent(site);
+                 output.Add(result);
+             });
+            return output;
+        }
+
+        public static async Task<List<WebsiteDataModel>> RunDownloadAsync(/*IProgress<ProgressReportModel> progress,*/ColorProgressBar.ColorProgressBar prb_loading, BackgroundWorker bw, CancellationToken cancellationToken)
+        {
+            List<string> webPages = Preparedata();
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+            ProgressReportModel report = new ProgressReportModel();
+            prb_loading.Maximum = 100;
+            prb_loading.Value = 0;
+            prb_loading.BarColor = Color.Aqua;
+            prb_loading.FillStyle = ColorProgressBar.ColorProgressBar.FillStyles.Solid;
+
+            foreach (var webpage in webPages)
+            {
+                WebsiteDataModel result = await Task.Run(() => DownloadWebContentAsync(webpage));
+                output.Add(result);
+
+                // this is not nessesary but i want to point it out as a option 
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                }
+
+                report.WebPagesDownloaded = output;
+                report.PercentCompleted = (output.Count * 100) / webPages.Count;
+                bw.ReportProgress(report.PercentCompleted, report);
+
+                // when we REPORT the progress it will raise ProgressChanged event and will call the methods
+                // who are attached
+                //if (progress != null)
+                //{
+                //    progress.Report(report);
+                //}
+            }
+
+            return output;
+        }
+
+        public static async Task<List<WebsiteDataModel>> RunDownloadParallelAsync()
+        {
+            List<string> webPages = Preparedata();
+            List<Task<WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
+
+            foreach (var webpage in webPages)
+            {
+                tasks.Add(DownloadWebContentAsync(webpage));
+            }
+            WebsiteDataModel[] output = await Task.WhenAll(tasks);
+
+            return new List<WebsiteDataModel>(output);
+        }
+
+        public static async Task<List<WebsiteDataModel>> RunDownloadParallelASyncV2(ColorProgressBar.ColorProgressBar prb_loading, BackgroundWorker bw)
+        {
+            List<string> webPages = Preparedata();
+            ProgressReportModel report = new ProgressReportModel();
+            List<WebsiteDataModel> output = new List<WebsiteDataModel>();
+            prb_loading.Maximum = 100;
+            prb_loading.Value = 0;
+            prb_loading.BarColor = Color.BurlyWood;
+            prb_loading.FillStyle = ColorProgressBar.ColorProgressBar.FillStyles.Dashed;
+
+            await Task.Run(() =>
+            {
+                Parallel.ForEach<string>(webPages, (site) =>
+                {
+                    WebsiteDataModel result = DownloadWebContent(site);
+                    lock (output)
+                    {
+                        output.Add(result);
+                        report.PercentCompleted = (output.Count * 100) / webPages.Count;
+                        report.WebPagesDownloaded = output;
+                        bw.ReportProgress(report.PercentCompleted, report);
+                    }
+
+                });
+            });
+            return output;
+        }
+
+        private static List<string> Preparedata()
+        {
+            List<string> listOfWebsites = new List<string>();
+
+            listOfWebsites.Add("https://www.google.com");
+            listOfWebsites.Add("https://www.microsoft.com");
+            listOfWebsites.Add("https://www.github.com");
+            listOfWebsites.Add("https://www.stackoverflow.com");
+            listOfWebsites.Add("https://www.yahoo.com");
+            listOfWebsites.Add("https://www.amazon.com");
+            return listOfWebsites;
+        }
+
+        private static WebsiteDataModel DownloadWebContent(string webPage)
+        {
+            WebsiteDataModel webData = new WebsiteDataModel();
+            WebClient webclient = new WebClient();
+            webData.WebsiteUrl = webPage;
+            webData.WedSiteData = webclient.DownloadString(webPage);
+            return webData;
+        }
+
+        private static async Task<WebsiteDataModel> DownloadWebContentAsync(string webPage)
+        {
+            WebsiteDataModel webData = new WebsiteDataModel();
+            WebClient webclient = new WebClient();
+            webData.WebsiteUrl = webPage;
+            webData.WedSiteData = await webclient.DownloadStringTaskAsync(webPage);
+            return webData;
+        }
+    }
+}
