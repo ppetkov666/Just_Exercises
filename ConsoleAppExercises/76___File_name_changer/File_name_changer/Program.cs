9349c2021059aaf9b703025230895a990ab9ea91
petko <ptpetkov666@gmail.com>
2019-02-17 22:02:45 +0200

    file name changer test

---
+++ ConsoleAppExercises/76___File_name_changer/File_name_changer/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace File_name_changer
+{
+    using System.IO;
+    public class Program
+    {
+        public const string Pattern = "___";
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\user\Desktop\Just_Exercises\ConsoleAppExercises";
+
+
+            string[] folders = Directory.GetDirectories(path);
+
+            for (int i = 0; i < folders.Length; i++)
+            {
+                string name = folders[i];
+                string folderName = Path.GetFileName(name);
+                string clearName = folderName.Substring(5);
+                string add = folderName.Substring(0,2);
+                string newName = clearName + Pattern + add;
+                int lenght = name.Length - folderName.Length;
+                string dir = name.Substring(0, lenght);
+                string newdir = dir + newName;
+                Directory.Move(name, newdir);
+            }
+        }
+    }
+}
