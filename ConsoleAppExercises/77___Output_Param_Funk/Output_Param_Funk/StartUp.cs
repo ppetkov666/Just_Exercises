5c7fbe931ed343f8f172492614d8d10fd3968737
petko <ptpetkov666@gmail.com>
2019-03-23 21:21:01 +0200

    output param example c#

---
+++ ConsoleAppExercises/77___Output_Param_Funk/Output_Param_Funk/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace Output_Param_Funk
+{
+    using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Test example = new Test();
+            int firstNumber = 10;
+            int secondNumber = 20;
+            Console.WriteLine($"firstNumber:{firstNumber}");
+            Console.WriteLine($"firstNumber:{secondNumber}");
+
+            example.GetNums(out firstNumber, out secondNumber);
+
+            Console.WriteLine($"Changed Value:{firstNumber}");
+            Console.WriteLine($"Changed Value:{secondNumber}");
+        }
+    }
+}
---
+++ ConsoleAppExercises/77___Output_Param_Funk/Output_Param_Funk/Test.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Output_Param_Funk
+{
+    using System;
+    public class Test
+    {
+        public void GetNums(out int firstNumber, out int secondNumber)
+        {
+            System.Console.WriteLine("Enter your first number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+            secondNumber = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
